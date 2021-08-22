cae2e69eb323f9c67295cccaaf5579e8f364aca2
Trent Park <trentpark8800@gmail.com>
2021-08-22 14:55:10 +0200

    Create a dropbox writer utils class

---
+++ src/dropbox_utils/dropbox_writer.py
@@ -0,0 +1,33 @@
+"""
+Module to write the processed data to dropbox.
+"""
+from configparser import ConfigParser
+from typing import Optional
+from dropbox import Dropbox
+
+
+class DropboxWriter:
+    def __init__(self, config_path: str, api_credentials_path: str) -> None:
+
+        self.config = ConfigParser()
+        self.config.read(config_path)
+
+        self.credentials: ConfigParser = ConfigParser()
+        self.credentials.read(api_credentials_path)
+
+        self.api = None
+        self.__config_api()
+
+    def __config_api(self) -> Dropbox:
+
+        token: str = self.credentials.get("DROPBOX", "token")
+
+        self.api = Dropbox(token)
+
+    def write_raw_file_to_dropbox(
+        self, data: str, file_name: str, encoding: Optional[str] = "utf-8"
+    ):
+        write_path = (
+            f"""{self.config.get("OUTPUT", "dropbox_write_path")}/{file_name}"""
+        )
+        self.api.files_upload(data.encode(encoding), path=write_path)
